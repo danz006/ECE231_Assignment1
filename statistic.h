38b88b045d46a2d21cb8165701cc1c361a182da5
jamm-24 <jziglar@unm.edu>
2025-09-13 10:48:55 -0600

    finish

---
+++ statistic.h
@@ -0,0 +1,13 @@
+#ifndef _STATISTICS.H
+#define _STATISTICS_H
+
+double calculateSum(const double arr[], int size);
+double calculateAverage(const double arr[], int size);
+double calculateDeviation(const double arr[], int size);
+
+	double cal_max;
+	double cal_min;
+	double cal_absolute_value;
+
+
+#endif
